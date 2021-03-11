a8976f56bdc28ec84f1ea081ed38a884e14f93e6
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2021-03-11 15:58:41 +0000

    use the module-results folder

--- component/parameter/directory.py
+++ component/parameter/directory.py
@@ -4,22 +4,29 @@ from pathlib import Path
 ##     directories      ##
 ##########################
 
+# this directory is the root directory of all sepal dashboard app.
+# please make sure that any result that you produce is embeded inside this folder 
+# create a folder adapted to your need inside this folder to save anything in sepal 
+module_dir = Path('~','module_results').expanduser()
+module_dir.mkdir(exist_ok=True)
+
+# create the general result dir
+result_dir = module_dir.joinpath('gwb_results')
+result_dir.mkdir(exist_ok = True)
+
 def get_result_dir(process):
     """get the result dir for each process"""
     
-    # create the general result dir
-    result_dir = Path('~', 'gwb_results').expanduser()
-    result_dir.mkdir(exist_ok = True)
-    
     # create the subresult dir 
-    result_dir = result_dir.joinpath(process)
-    result_dir.mkdir(exist_ok = True)
+    dir_ = result_dir.joinpath(process)
+    dir_.mkdir(exist_ok = True)
     
-    return result_dir
+    return dir_
     
 def get_tmp_dir():
     """get or create the tmp dir corresponding to each process"""
-    tmp_dir = Path('~', 'gwb_results', 'tmp').expanduser()
+    
+    tmp_dir = result_dir.joinpath('tmp')
     tmp_dir.mkdir(exist_ok = True)
     
     return tmp_dir
