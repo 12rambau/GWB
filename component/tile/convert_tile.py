4b5ed0f5d18e9a862208e30038f7532eda6deb68
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2021-04-08 12:55:34 +0000

    catch error when no file is created

--- component/tile/convert_tile.py
+++ component/tile/convert_tile.py
@@ -20,7 +20,10 @@ class ConvertByte(sw.Tile):
             items = None, 
             v_model = None, 
             chips = True, 
-            multiple = True
+            small_chips = True,
+            multiple = True,
+            dense = True,
+            deletable_chips = True
         ) for i in range(len(cp.convert[nb_class]['label']))]
         requirements = sw.Markdown(cm.requirement[nb_class])
         
@@ -51,28 +54,25 @@ class ConvertByte(sw.Tile):
             
         # check variables
         if not self.output.check_input(self.io.file, cm.bin.no_file): return widget.toggle_loading()
-        #if not self.output.check_input(len(self.io.foreground), cm.bin.no_classes): return widget.toggle_loading()
             
         # compute the bin map
-        #try:
+        try:
         
-        # update byte list 
-        self.io.update_byte_list()
+            # update byte list 
+            self.io.update_byte_list()
         
-        # create a bin map 
-        bin_map = cs.set_byte_map(
-            self.io.byte_list, 
-            self.io.file, 
-            self.io.process, 
-            self.output
-        )
+            # create a bin map 
+            bin_map = cs.set_byte_map(
+                self.io.byte_list, 
+                self.io.file, 
+                self.io.process, 
+                self.output
+            )
             
-        self.io.set_bin_map(bin_map)
+            self.io.set_bin_map(bin_map)
             
-        # add the bin map to the download btn
-            
-        #except Exception as e:
-        #    self.output.add_live_msg(str(e), 'error')
+        except Exception as e:
+            self.output.add_live_msg(str(e), 'error')
                 
             
         # release the btn 
