ff7bfc7d47bdf6c8763fd0361c02b474bc4d6691
dfguerrerom <dfgm2006@gmail.com>
2024-06-25 14:55:41 +0200

    ci: return none if there's no version. This will be done when the test is done in github actions

--- component/scripts/gwb_version.py
+++ component/scripts/gwb_version.py
@@ -4,17 +4,23 @@ import re
 
 def get_gwb_version():
     """Get the version of GWB installed on the system and write it to CHANGELOG.md."""
-    # Run the command and capture output
-    result = subprocess.run(["GWB"], capture_output=True, text=True)
-    output = result.stdout.splitlines()
 
-    # Find and capture the version number
-    version = None
-    for line in output:
-        match = re.search(r"Installed version: (\d+\.\d+\.\d+)", line)
-        if match:
-            version = match.group(1)
-            break
+    try:
+        # Run the command and capture output
+        result = subprocess.run(["GWB"], capture_output=True, text=True)
+        output = result.stdout.splitlines()
+
+        # Find and capture the version number
+        version = None
+        for line in output:
+            match = re.search(r"Installed version: (\d+\.\d+\.\d+)", line)
+            if match:
+                version = match.group(1)
+                break
+
+    except Exception as e:
+        version = "There is no GWB installation on this system."
+        print(e)
 
     return version or "There is no GWB installation on this system."
 
